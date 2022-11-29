cb784d2d948d50ca91e80db6141466b994b816b2
Arsen <ak_val@bk.ru>
2022-11-30 02:50:27 +0300

    Добавлено решение к задаче №23

---
+++ Task23.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов от 1 до N
+
+/*
+void Task19()
+{
+    Console.WriteLine("Введите положительное число больше нуля, до которого предыдущие числа будут возводиться в куб");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    if (number <= 0 )
+    {
+        Console.WriteLine("Введено отрицательное число или 0. Необходимо ввести положительное число");
+    }
+    else
+    {
+        Console.WriteLine("Число, до которого предыдущие будут возводиться в куб: " +number);
+        Console.WriteLine();
+        Console.WriteLine("Число" + "\t" + "Куб");
+        for (int counter = 1; counter <= number; counter++)
+        {
+            Console.WriteLine(counter + "\t" + $"{Math.Pow(counter, 3)}");
+        }
+    }
+}
+Task19();
+*/
